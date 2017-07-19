ebbe43f82a4c0c67e78647e9559f5f15a23f779a
TuanaCelik <tc13007@my.bristol.ac.uk>
2017-07-19 11:29:14 +0300

    no message

--- server.js
+++ server.js
@@ -17,10 +17,10 @@ connection.query('SELECT 1 + 1 AS solution', function (err, rows, fields) {
 
   //console.log('The solution is: ', rows[0].solution);
   console.log("You are now connected...")
-  /*connection.query('DESCRIBE events', function(err, result){
+  connection.query('DESCRIBE events', function(err, result){
   	if (err) throw err
   	console.log(result)
-  })*/
+  })
 
 })
 /*app.listen(3000);
